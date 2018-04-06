abebb56b654a001f36119f462765537a211703f0
Kurt Junghanns <tboonx@googlemail.com>
2018-04-06 15:13:40 +0200

    [master] Save work

--- application/manageAPIs.js
+++ application/manageAPIs.js
@@ -17,6 +17,7 @@ Detailed:
  *
 */
 
+//get domains from containers
 let domains = containers.reduce((s, container) => {
   if (container.State && container.State.Running && container.Env && container.Env.LETSENCRYPT_HOST) {
     if (!s.has(container.Env.LETSENCRYPT_HOST))
@@ -27,9 +28,16 @@ let domains = containers.reduce((s, container) => {
 
 console.log('Detected ' + domains.size + ' domains from containers:', domains);
 
+let newAPIs = [];
+let obsoleteAPIs = [];
+let promises = [];
+let lastAPIs = [];
+let currentCertificates = [];
+let apisForWhichACertificateIsNeeded = [];
 kongAPI.listAPIs() //NOTE dont forget that entry could have routes as attribute
+  //delete not needed services and routes (just lets encrypt ones) (first routes via /services/{service name or id}/routes and then the services)
+  //Also get new apis
   .then((apis) => {
-    let newAPIs = [];
     containers.forEach((container) => {
       if (!container.State || !container.State.Running || !container.Env || !container.Env.LETSENCRYPT_HOST)
         return;
@@ -46,7 +54,6 @@ kongAPI.listAPIs() //NOTE dont forget that entry could have routes as attribute
     });
     console.log('Found '+newAPIs.length+' new containers (for new APIs)');
 
-    let obsoleteAPIs = [];
     (apis || []).forEach((api) => {
       let found = false;
       containers.forEach((container) => {
@@ -61,117 +68,116 @@ kongAPI.listAPIs() //NOTE dont forget that entry could have routes as attribute
     });
     console.log('Found '+obsoleteAPIs.length+' obsolete APIs');
 
-    let promises = [];
     obsoleteAPIs.forEach((api) => {
       promises.push(kongAPI.deleteUpstreamHost(api.id));
     });
-    Promise.all(promises)
-      .then(() => {
-        //update upstream URLs
-        return kongAPI.listAPIs() //NOTE dont forget that entry could have routes as attribute
-          .then((apis) => {
-            let changedUpstreams = [];
-            containers.forEach((container) => {
-              if (!container.State || !container.State.Running || !container.Env || !container.Env.LETSENCRYPT_HOST)
-                return;
-              let found = false;
-              (apis || []).forEach((api) => {
-                if (api.domain && container.Env.LETSENCRYPT_HOST === api.domain) {
-                  found = api;
-                  return;
-                }
-              });
-
-              if (found && container.IP !== found.service.host) {
-                console.log('Found mismatching IP of service:', found.service, container);
-                changedUpstreams.push({container: container, api: found});
-              }
-            });
-            console.log('Found '+changedUpstreams.length+' APIs with outdated upstream');
-
-            promises = [];
-            changedUpstreams.forEach((entry) => {
-              promises.push(kongAPI.updateService(entry.api.service.id, LetsEncryptPrefix + entry.container.Env.LETSENCRYPT_HOST, 'http://' + entry.container.IP));
-            });
-            newAPIs.forEach((container) => {
-              promises.push(kongAPI.addUpstreamHost(container.Env.LETSENCRYPT_HOST, 'http://'+container.IP));
-            });
-
-            Promise.all(promises)
-              .then(() => {
-                return kongAPI.listCertificates()
-                  .then((certificates) => {
-                    let apisForWhichACertificateIsNeeded = [].concat(newAPIs);
-                    certificates.forEach((certificate) => {
-                      const domain = certificate.snis[0];
-                      let matchingApi = apis.find((api) => return api.route.hosts[0] === domain);
-                      if (matchingApi) {
-                        const now = (new Date()).getTime();
-                        if (now > certificate.created_at + 89*24*60*60*1000) { //89 days
-                          apisForWhichACertificateIsNeeded.push(matchingApi);
-                        }
-                      }
-                    });
-
-                    //create service and route for companion
-                    return kongAPI.addCompanionAPI('http://' + companionIP);
-                  })
-                  .then((data) => {
-                    promises = [];
-                    certificates.forEach((certificate) => {
-                      if (!domains.has(certificate.snis[0]))
-                        promises.push(kongAPI.deleteCertificate(certificate.id));
-                    });
-                    return Promise.all(promises);
-                  })
-                  .then(() => {
-                    console.log('Removed', promises.length-1, 'Certificates!');
-
-                    // build command string
-                    const domainsForCertbot = apisForWhichACertificateIsNeeded.reduce((a, curr) => {
-                      if (a === '')
-                        return curr.domain;
-                      return a + ',' + curr.domain;
-                    }, '');
-                    let cmd = 'certbot certonly --agree-tos --standalone --preferred-challenges http -n -m ' + EMAIL + ' --expand -d ' + domainsForCertbot;
-
-                    console.log('Now executing the certbot command:', cmd);
-                    const certbot_log = require('child_process').execSync(cmd);
-                    //TODO check log
-                    console.log('The command returned:', certbot_log);
-
-                    promises = [];
-                    const path = '/etc/letsencrypt/live';
-                    apisForWhichACertificateIsNeeded.forEach((api) => {
-                      promises.push(kongAPI.addCertificate(api-domain, path));
-                    });
-
-                    return Promise.all(promises);
-                  })
-                  .then(() => {
-                    console.log('Added', promises.length, 'Certificates!');
-
-
-                  })
-                  .catch((error) => {
-                    console.log('Error', error);
-                    process.exit(0);
-                  });
-              })
-              .catch((reason) => {
-                console.log('Failed to update APIs: ', reason);
-                process.exit(0);
-              });
-          })
-          .catch((error) => {
-            console.log('Error', error);
-            process.exit(0);
-          });
-      })
-      .catch((reason) => {
-        console.log('Failed delete old APIs: ', reason);
-        process.exit(0);
+    return Promise.all(promises);
+  })
+  .then(() => {
+    return kongAPI.listAPIs(); //NOTE dont forget that entry could have routes as attribute
+  })
+  //update changed upstreams (just lets encrypt ones)
+  //and create services and route for the missing domains
+  .then((apis) => {
+    lastAPIs = apis;
+    let changedUpstreams = [];
+    containers.forEach((container) => {
+      if (!container.State || !container.State.Running || !container.Env || !container.Env.LETSENCRYPT_HOST)
+        return;
+      let found = false;
+      (apis || []).forEach((api) => {
+        if (api.domain && container.Env.LETSENCRYPT_HOST === api.domain) {
+          found = api;
+          return;
+        }
       });
+
+      if (found && container.IP !== found.service.host) {
+        console.log('Found mismatching IP of service:', found.service, container);
+        changedUpstreams.push({container: container, api: found});
+      }
+    });
+    console.log('Found '+changedUpstreams.length+' APIs with outdated upstream');
+
+    promises = [];
+    changedUpstreams.forEach((entry) => {
+      promises.push(kongAPI.updateService(entry.api.service.id, LetsEncryptPrefix + entry.container.Env.LETSENCRYPT_HOST, 'http://' + entry.container.IP));
+    });
+    newAPIs.forEach((container) => {
+      promises.push(kongAPI.addUpstreamHost(container.Env.LETSENCRYPT_HOST, 'http://'+container.IP));
+    });
+
+    return Promise.all(promises);
+  })
+  .then(() => {
+    return kongAPI.listCertificates();
+  })
+  //add to list of new apis, apis which need a new certificate
+  //create a service and a route for the companion
+  .then((certificates) => {
+    currentCertificates = certificates;
+    apisForWhichACertificateIsNeeded = [].concat(newAPIs);
+    certificates.forEach((certificate) => {
+      const domain = certificate.snis[0];
+      let matchingApi = lastAPIs.find((api) => return api.route.hosts[0] === domain);
+      if (matchingApi) {
+        const now = (new Date()).getTime();
+        if (now > certificate.created_at + 89*24*60*60*1000) { //89 days
+          apisForWhichACertificateIsNeeded.push(matchingApi);
+        }
+      }
+    });
+
+    //create service and route for companion
+    return kongAPI.addCompanionAPI('http://' + companionIP);
+  })
+  //delete unneeded certificates
+  .then((data) => {
+    promises = [];
+    currentCertificates.forEach((certificate) => {
+      if (!domains.has(certificate.snis[0]))
+        promises.push(kongAPI.deleteCertificate(certificate.id));
+    });
+    return Promise.all(promises);
+  })
+  //get the certificates
+  .then(() => {
+    console.log('Removed', promises.length-1, 'Certificates!');
+
+    // build command string
+    const domainsForCertbot = apisForWhichACertificateIsNeeded.reduce((a, curr) => {
+      if (a === '')
+        return curr.domain;
+      return a + ',' + curr.domain;
+    }, '');
+    let cmd = 'certbot certonly --agree-tos --standalone --preferred-challenges http -n -m ' + EMAIL + ' --expand -d ' + domainsForCertbot;
+
+    console.log('Now executing the certbot command:', cmd);
+    const certbot_log = require('child_process').execSync(cmd);
+    //TODO check log
+    console.log('The command returned:', certbot_log);
+
+    return Promise.resolve();
+  })
+  //Add certificates with domain to Kong
+  .then(() => {
+    promises = [];
+    const path = '/etc/letsencrypt/live';
+    apisForWhichACertificateIsNeeded.forEach((api) => {
+      promises.push(kongAPI.addCertificate(api-domain, path));
+    });
+
+    return Promise.all(promises);
+  })
+  //delete service and the route of the companion
+  .then(() => {
+    console.log('Added', promises.length, 'Certificates!');
+
+    return kongAPI.deleteUpstreamHost('kong-companion');
+  })
+  .then(() => {
+    console.log('Success!');
+    process.exit(0);
   })
   .catch((error) => {
     console.log('Error', error);
@@ -190,7 +196,7 @@ kongAPI.listAPIs() //NOTE dont forget that entry could have routes as attribute
   //create a service and a route for the companion
   //delete unneeded certificates
   //get the certificates
-  //Add certificates with domain to Kong (perhaps add the end?)
+  //Add certificates with domain to Kong (perhaps at the end?)
   //delete service and the route of the companion
 
 
